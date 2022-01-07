db12cf9f6c17473536feeb1730589493b66a4e33
Ventura Lucena Martínez <ventura_lucena_99@hotmail.com>
2022-01-07 12:00:31 +0100

    Assignment 3

--- Assignment_3/assoc.py
+++ Assignment_3/assoc.py
@@ -1,28 +1,30 @@
 #!  /bin/python3
 
+from apyori import apriori
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv('store_data.csv')
+dataset = pd.read_csv('datasets/store_data.csv')
 print(dataset.shape)
 
 # Transforming the list into a list of lists, so that each transaction can be indexed easier
 transactions = []
 for i in range(0, dataset.shape[0]):
-    transactions.append([str(dataset.values[i, j]) for j in range(0, dataset.shape[1])])
+    transactions.append([str(dataset.values[i, j])
+                        for j in range(0, dataset.shape[1])])
 
 
-from apyori import apriori
 # Please download this as a custom package --> type "apyori"
 # To load custom packages, do not refresh the page. Instead, click on the reset button on the Console.
 
-rules = apriori(transactions, min_support = 0.005, min_confidence = 0.25, min_lift = 3, min_length = 2)
+rules = apriori(transactions, min_support=0.005,
+                min_confidence=0.25, min_lift=3, min_length=2)
 # Support: number of transactions containing set of times / total number of transactions
 # .      --> products that are bought at least 3 times a day --> 21 / 7501 = 0.0027
 # Confidence: Should not be too high, as then this wil lead to obvious rules
 
-#Try many combinations of values to experiment with the model
+# Try many combinations of values to experiment with the model
 
 results = []
 for item in rules:
@@ -33,11 +35,11 @@ for item in rules:
     items = [x for x in pair]
     print("Rule: " + items[0] + " -> " + items[1])
 
-    #second index of the inner list
+    # second index of the inner list
     print("Support: " + str(item[1]))
 
-    #third index of the list located at 0th
-    #of the third index of the inner list
+    # third index of the list located at 0th
+    # of the third index of the inner list
 
     print("Confidence: " + str(item[2][0][2]))
     print("Lift: " + str(item[2][0][3]))
